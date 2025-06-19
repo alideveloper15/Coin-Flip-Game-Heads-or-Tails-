ee1aad69375f562eca2ce5a4d735e0d6708a4192
alideveloper15 <172058104+alideveloper15@users.noreply.github.com>
2025-06-19 10:30:05 +0500

    Added DOM and score card

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+    wins: 0,
+    losses: 0,
+}
+
+updateScoreElement()
+
+
+function playGame(playerMove) {
+    const computerMove = pickComputerMove()
+    const guess = playerMove;
+    if (guess === computerMove) {
+        result = 'You win!'
+    }
+    else {
+        result = 'You lose!'
+    }
+    
+    
+    
+    if (result === 'You win!') {
+        score.wins += 1
+    }
+    else if (result === 'You lose!') {
+        score.losses += 1
+    }
+    
+    
+    localStorage.setItem('score', JSON.stringify(score))
+    
+    document.querySelector('.js-result').innerHTML = result
+    
+    document.querySelector('.js-moves')
+    .innerHTML = `You ${playerMove} - ${computerMove} Computer`
+    
+    updateScoreElement()
+}
+
+
+function updateScoreElement() {
+    document.querySelector('.js-score')
+        .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}`
+}
+
 
 function pickComputerMove() {
     const randomNumber = Math.random();
-    result = randomNumber < 0.5 ? 'heads' : 'tails';
-}
-
-function playGame(playerMove) {
-    pickComputerMove()
-    const guess = playerMove;
-    alert(guess === result ? 'You win!' : 'You lose!');
+    let computerMove = ''
+    if (randomNumber < 0.5) {
+        computerMove = 'heads'
+    }
+    else {
+        computerMove = 'tails'
+    }
+    return computerMove
 }
